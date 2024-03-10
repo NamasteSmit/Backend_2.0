e1ce555d29137d2916f0c3496ec9206fd8409b48
NamasteSmit <smitpatel1305@gmail.com>
2024-03-10 15:12:01 -0400

    connecting to db

---
+++ dynamicBackend.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://smitpatel1305:Smit2004@cluster0.udla4ro.mongodb.net/newuser_app");
+
+const users = mongoose.model('Users',{
+    name:String,
+    email:String,
+    age:Number
+})
+
+
+app.use(express.json());
+app.post('/signup',async (req,res)=>{
+    const name = req.body.name;
+    const email = req.body.email;
+    const age = req.body.age;
+
+    const existingUser = await users.findOne({ email: email});
+    console.log(existingUser);
+    if(existingUser){
+        return res.status(400).json({
+            message : "User already exist"
+        })
+    }
+
+    const user = new users({
+        name: name,
+        email: email,
+        age: age
+    })
+
+    user.save();
+    res.json({
+        message : "User added"
+    })
+})
+
+app.listen(port,(req,res)=>{
+    console.log("Server is running on port",port);
+})
